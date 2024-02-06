add462d19afcfc98760220272e319cb475018572
Wasim <er.wasim339@gmail.com>
2024-02-06 19:19:06 +0530

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Fitness from './pages/Fitness'
-import Dropdown from './components/Dropdown'
+
 
 // font 
 import "@fontsource/metropolis"; // Defaults to weight 400
@@ -14,21 +12,11 @@ import "@fontsource/metropolis/700.css"; // Specify weight
 import "@fontsource/metropolis/400-italic.css"; // Specify weight and style
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  // const [fontsLoaded] = useFonts({
-  //   Metropolis: require("./assets/fonts/metropolis.regular.otf"),
-  // });
-
-  // if (!fontsLoaded) {
-  //   return undefined;
-  // }
 
 
 
   return (
     <>
-      {/* <Dropdown /> */}
       <Fitness />
 
     </>
--- src/pages/Fitness.jsx
+++ src/pages/Fitness.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./Fitness.css";
+
+// images 
 import arrow from "../assets/arrow.png";
 import user from "../assets/user.png";
 import watch from "../assets/watch.png";
@@ -7,16 +9,14 @@ import down from "../assets/down.png";
 import circle from "../assets/circle.png";
 import half from "../assets/half.png";
 
-
 const Fitness = () => {
-
     // dropdown
-    const [isOpen, setIsOpen] = useState(false)
-    const [select, setSelect] = useState('')
+    const [isOpen, setIsOpen] = useState(false);
+    const [select, setSelect] = useState("");
 
     const dropdownHandler = () => {
         // setIsOpen(!isOpen)
-    }
+    };
     return (
         <div className="container">
             <div className="fitnessWrapper">
@@ -31,20 +31,24 @@ const Fitness = () => {
 
                 {/* your fitness trakcer  */}
                 <div className="tracker">
-                    <div className="trackerText"><h1>your <br /> fitness trakcer </h1></div>
+                    <div className="trackerText">
+                        <h1>
+                            your <br /> fitness trakcer{" "}
+                        </h1>
+                    </div>
 
                     <div className="devicesContainer">
                         <div className="device">
                             <h1>Connected devices</h1>
                         </div>
                         <div className="statistic">
-                            <h1>statistic</h1></div>
+                            <h1>statistic</h1>
+                        </div>
                     </div>
                 </div>
 
                 {/* wathc card  */}
                 <div className="watchContainer">
-
                     <div className="pinkContainer">.</div>
                     <div className="lightPinkContainer">.</div>
                     <div className="black">
@@ -53,27 +57,21 @@ const Fitness = () => {
                             <h3>Fire Bolt</h3>
                         </div>
                         <div className="blackContainerCenter">
-                            <p>Scan the device <br /> QR to connect</p>
-
+                            <p>
+                                Scan the device <br /> QR to connect
+                            </p>
                         </div>
                         <div className="blackContainerBottom">
                             <div className="scan">
                                 <p>Scan</p>
-
                             </div>
                         </div>
                         <div className="watchImage">
                             <img src={watch} alt="" />
                         </div>
-
                     </div>
-                    <div className="blur">
-
-                    </div>
-
-
+                    <div className="blur"></div>
                 </div>
-
 
                 <div className="trackerContainer">
                     <div className="trackerLeft">
@@ -85,26 +83,20 @@ const Fitness = () => {
                                 <p>Steps</p>
                             </div>
                             <img className="halfImage" src={half} alt="" />
-
                         </div>
                     </div>
                     <div className="trackerRight">
                         <div className="calorires">
                             <p>Calories</p>
                             <h5>1200 kcal</h5>
-
                         </div>
                         <div className="calorires">
                             <p>Water</p>
                             <h5>1.8 Ltrs</h5>
-
                         </div>
-
                     </div>
                 </div>
-
             </div>
-
 
             {/* details  */}
             <div className="detailContainer">
@@ -120,61 +112,79 @@ const Fitness = () => {
                             <h5>Are you a gym member?</h5>
                             <h1>{select}</h1>
                             <img src={down} alt="" />
-
                         </div>
                         {isOpen ? (
                             <div className="dropdownItems">
-                                <li value={select} onChange={(e) => setSelect(e.target.value)} className="dropdownItems">Yes</li>
-                                <li value={select} onChange={(e) => setSelect(e.target.value)} className="dropdownItems">No</li>
+                                <li
+                                    value={select}
+                                    onChange={(e) => setSelect(e.target.value)}
+                                    className="dropdownItems"
+                                >
+                                    Yes
+                                </li>
+                                <li
+                                    value={select}
+                                    onChange={(e) => setSelect(e.target.value)}
+                                    className="dropdownItems"
+                                >
+                                    No
+                                </li>
                             </div>
-
-
                         ) : null}
-
-
                     </div>
                     <div className="dropdown">
                         <div className="dropdownMenu" onClick={dropdownHandler}>
                             <h5>Do you eat meat?</h5>
                             <h1>{select}</h1>
                             <img src={down} alt="" />
-
                         </div>
                         {isOpen ? (
                             <div className="dropdownItems">
-                                <li value={select} onChange={(e) => setSelect(e.target.value)} className="dropdownItems">Yes</li>
-                                <li value={select} onChange={(e) => setSelect(e.target.value)} className="dropdownItems">No</li>
+                                <li
+                                    value={select}
+                                    onChange={(e) => setSelect(e.target.value)}
+                                    className="dropdownItems"
+                                >
+                                    Yes
+                                </li>
+                                <li
+                                    value={select}
+                                    onChange={(e) => setSelect(e.target.value)}
+                                    className="dropdownItems"
+                                >
+                                    No
+                                </li>
                             </div>
-
-
                         ) : null}
-
-
                     </div>
                     <div className="dropdown">
                         <div className="dropdownMenu" onClick={dropdownHandler}>
                             <h5>Are you a cyclist?</h5>
                             <h1>{select}</h1>
                             <img src={down} alt="" />
-
                         </div>
                         {isOpen ? (
                             <div className="dropdownItems">
-                                <li value={select} onChange={(e) => setSelect(e.target.value)} className="dropdownItems">Yes</li>
-                                <li value={select} onChange={(e) => setSelect(e.target.value)} className="dropdownItems">No</li>
+                                <li
+                                    value={select}
+                                    onChange={(e) => setSelect(e.target.value)}
+                                    className="dropdownItems"
+                                >
+                                    Yes
+                                </li>
+                                <li
+                                    value={select}
+                                    onChange={(e) => setSelect(e.target.value)}
+                                    className="dropdownItems"
+                                >
+                                    No
+                                </li>
                             </div>
-
-
                         ) : null}
-
-
                     </div>
                     <button className="saveBtn">Save Details</button>
                 </form>
-
             </div>
-
-
         </div>
     );
 };
